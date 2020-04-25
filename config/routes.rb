46a7714871af900dbe8559b627a9f1ada60773ef
takepon <tqr1104@yahoo.co.jp>
2020-04-25 19:21:28 +0900

    devise導入＆新規登録機能の開発

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
+  devise_for :users
+  root 'home#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
