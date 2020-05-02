936f2269cc49a765b42953c5a74fa8c24c1f0901
takepon <tqr1104@yahoo.co.jp>
2020-05-02 17:11:25 +0900

    ログイン後のページ制限

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def index
+    if user_signed_in?
+      redirect_to users_top_path 
+    end
+  end
 end
