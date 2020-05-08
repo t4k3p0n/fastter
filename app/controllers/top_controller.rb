81d7d5dff447e6106acaa9715ad028b8bf742dac
takepon <tqr1104@yahoo.co.jp>
2020-05-09 02:39:28 +0900

    ユーザー検索機能完成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,12 @@
 class TopController < ApplicationController
     def index
         @users = User.all
-        
+        @q = User.ransack(params[:q])
+        @user = @q.result(distinct: true)
       end
 
     def show
+    @users = User.all
     @q = User.ransack(params[:q])
     @user = @q.result(distinct: true)
     end
