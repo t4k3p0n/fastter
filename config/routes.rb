89a299eb69cf8bcdc01656c1a4c4fb1f586eef80
takepon <tqr1104@yahoo.co.jp>
2020-05-11 01:27:07 +0900

    comment関連作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,27 @@ Rails.application.routes.draw do
   get 'posts/show'
   get 'users/top'
   get 'top/show'
+
   root 'home#index'
+
   post '/users/guest_sign_in', to: 'users#new_guest'
+
   devise_for :users
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     post 'login', to: 'devise/sessions#create'
     delete 'signout', to: 'devise/sessions#destroy'
   end
+  
   resources :users, :only => [:show]
   resources :top, :only => [:index]
   resources :posts, only: [:index, :show, :create]
+  resources :relationships,only: [:create, :destroy]
+  resources :comments, only: [:create]
   resources :users do
     member do
      get :following, :followers
     end
   end
-  resources :relationships,       only: [:create, :destroy]
 
 end
