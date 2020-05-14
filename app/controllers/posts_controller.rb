d0516c114721266eded1dce4d21b9a67ce9645d3
takepon <tqr1104@yahoo.co.jp>
2020-05-14 22:07:16 +0900

    いいねのランキング機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,8 @@ class PostsController < ApplicationController
 
   def allpost
     @posts = Post.all
+
+    @post_ranking = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def show
