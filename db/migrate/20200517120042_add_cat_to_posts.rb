2c1b69ca39015275c471d3428b7a1edf844fb388
takepon <tqr1104@yahoo.co.jp>
2020-05-17 22:07:24 +0900

    Rspec導入

---
+++ db/migrate/20200517120042_add_cat_to_posts.rb
@@ -0,0 +1,5 @@
+class AddCatToPosts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :cat, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_041131) do
+ActiveRecord::Schema.define(version: 2020_05_17_120042) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_041131) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cat"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
