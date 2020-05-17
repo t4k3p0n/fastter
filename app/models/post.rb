da71ff62fc4f93f454b01f1215bb18bea38f322d
takepon <tqr1104@yahoo.co.jp>
2020-05-17 13:18:43 +0900

    いいねの通知メソッド作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,22 @@ class Post < ApplicationRecord
   def user
     return User.find_by(id: self.user_id)
   end
+
+  def create_notification_like!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
