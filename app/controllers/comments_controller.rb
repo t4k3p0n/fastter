c88084ce76e4a6eebef00cb7a660cb6e207a28a8
takepon <tqr1104@yahoo.co.jp>
2020-05-17 13:24:40 +0900

    各種通知呼び出し処理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
     def create
         @comment = Comment.new(comment_params)
         @comment.user_id = current_user.id
+        @post = @comment.post
         if @comment.save
+          @post.create_notification_comment!(current_user, @comment.id)
           redirect_back(fallback_location: root_path)
         else
           redirect_back(fallback_location: root_path)
